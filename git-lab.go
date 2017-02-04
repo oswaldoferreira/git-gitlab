5be3c8c6137f97da83945c87a89e9d09b7e169bc
Oswaldo Ferreira <oswluizf@gmail.com>
2017-02-04 18:14:01 -0200

    Apply go fmt command on the entire project

--- git-lab.go
+++ git-lab.go
@@ -13,9 +13,7 @@ func main() {
 	app.Usage = "command line tool that wrap git in order to extend it with extra features and commands that make working with GitLab easier."
 	app.Author = "numa08"
 	app.Email = "n511287@gmail.com"
-    app.Commands = Commands
-    
+	app.Commands = Commands
+
 	app.Run(os.Args)
 }
-
-
